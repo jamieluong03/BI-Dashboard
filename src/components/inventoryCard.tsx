613ae68863922d3ff9b6b677314054b152c63f2d
jamieluong03 <jamieluong03@gmail.com>
2026-04-03 17:31:29 -0700

    updated inventorycard styling for mobile

--- src/components/inventoryCard.tsx
+++ src/components/inventoryCard.tsx
@@ -25,18 +25,18 @@ export function InventoryCard({ title, inventoryValue, sellThroughRate, lowStock
       <CardContent>
         <div className="flex justify-around mb-2">
           <div>
-            <div className="text-lg text-slate-500 font-medium tracking-tight">
+            <div className="text-md md:text-lg text-slate-500 font-medium tracking-tight">
               Total Inventory Value
             </div>
-            <div className="text-xl font-bold text-slate-900 tracking-tight mb-2">
+            <div className="text-lg md:text-xl font-bold text-slate-900 tracking-tight mb-2">
               {currencyFormatter.format(inventoryValue)}
             </div>
           </div>
           <div>
-            <div className="text-lg text-slate-500 font-medium tracking-tight">
+            <div className="text-md md:text-lg text-slate-500 font-medium tracking-tight">
               Sell Through Rate
             </div>
-            <div className="text-xl font-bold text-slate-900 tracking-tight mb-2">
+            <div className="text-lg md:text-xl  font-bold text-slate-900 tracking-tight mb-2">
               {sellThroughRate}%
             </div>
           </div>
